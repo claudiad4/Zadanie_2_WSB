c81e5425ac651d411b56a04ec1a42a2c61a32446
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-11 23:26:16 +0200

    Add files via upload

---
+++ Zadanie_7/Zadanie_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Pary liczb naturalnych, których suma wynosi {0}:", n);
+
+        for (int i = 1; i <= n / 2; i++)
+        {
+            int j = n - i;
+            Console.WriteLine("{0} + {1} = {2}", i, j, n);
+        }
+    }
+}
---
+++ Zadanie_8/Zadanie_8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int min = 1;
+        int max = 20;
+        int zgadywanaLiczba;
+        int odpowiedz;
+
+        Console.WriteLine("Pomyśl liczbę z przedziału 1-20.");
+        Console.WriteLine("Naciśnij dowolny klawisz, gdy będziesz gotowy.");
+        Console.ReadKey();
+
+        do
+        {
+            zgadywanaLiczba = (min + max) / 2;
+            Console.WriteLine("Czy Twoja liczba to " + zgadywanaLiczba + "?");
+            Console.WriteLine("Wpisz -1, jeśli jest mniejsza, 1, jeśli jest większa, 0 jeśli zgadłem:");
+            odpowiedz = int.Parse(Console.ReadLine());
+
+            if (odpowiedz == -1)
+            {
+                max = zgadywanaLiczba - 1;
+            }
+            else if (odpowiedz == 1)
+            {
+                min = zgadywanaLiczba + 1;
+            }
+            else if (odpowiedz == 0)
+            {
+                Console.WriteLine("Odgadłem! Twoja liczba to " + zgadywanaLiczba + ".");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowa odpowiedź. Spróbuj jeszcze raz.");
+            }
+        } while (true);
+    }
+}
